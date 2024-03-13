6b4b45abe143835b0222332f9ae723cbd84cd452
AbdurRohit <sheikhabdurrohit@gmail.com>
2024-03-13 10:45:04 +0530

    added

---
+++ myApp/src/app/parameters/parameters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parameters',
+  standalone: true,
+  imports: [],
+  templateUrl: './parameters.component.html',
+  styleUrl: './parameters.component.css'
+})
+export class ParametersComponent {
+
+}
---
+++ myApp/src/app/shared/electrical-machine-parameter.ts
@@ -0,0 +1,8 @@
+export interface ElectricalMachineParameter {
+    id: number; 
+    name: string;
+    category: string; 
+    value: string;
+    unit: string;
+  }
+  
