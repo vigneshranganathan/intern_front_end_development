22ff0c0dfdbe23d0e02d39388b7058798bc13601
AbdurRohit <sheikhabdurrohit@gmail.com>
2024-03-17 07:56:30 +0530

    Data component added

--- myApp/src/app/app.config.ts
+++ myApp/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
---
+++ myApp/src/app/data-display/data-display.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-data-display',
+  templateUrl: './data-display.component.html',
+  styleUrls: ['./data-display.component.css']
+})
+export class DataDisplayComponent implements OnInit {
+  data: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+  }
+
+  fetchData(): void {
+    this.dataService.getData().subscribe(
+      (response) => {
+        this.data = response;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+}
