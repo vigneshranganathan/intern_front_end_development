18491a2f2902483fc54c1efe6638e0d953fb1d30
AbdurRohit <sheikhabdurrohit@gmail.com>
2024-03-18 13:12:38 +0530

    Fetch API working on angular

--- myApp/src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
-};
